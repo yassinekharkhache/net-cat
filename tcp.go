36616c59ff37aa4223845f2b10f377d5c550dbda
yassinekharkhache <judpak71@gmail.com>
2025-11-13 17:47:25 +0100

    little edits

--- tcp.go
+++ tcp.go
@@ -6,7 +6,7 @@ import (
 	"net"
 )
 
-var users []net.Conn
+var Users = make(map[string]net.Conn)
 
 func main() {
 	listner, err := net.Listen("tcp", ":8080")
@@ -14,20 +14,36 @@ func main() {
 		fmt.Println(err)
 	}
 	for {
+		Name := []byte{}
 		con, err := listner.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		go con_handler(con)
+		con.Write([]byte("write user name : "))
+		_, err = con.Read(Name)
+		if err != nil {
+			continue
+		}
+		Users[string(Name)] = con
+		go con_handler(string(Name))
 	}
 }
 
-func con_handler(con net.Conn) {
+func con_handler(name string) {
+	con := Users[name]
 	defer con.Close()
-	con.Write([]byte("write user name : "))
 	scanner := bufio.NewScanner(con)
+	msg := []byte{}
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		con.Read(msg)
+		go printMessage(string(msg), name)
+	}
+}
+
+func printMessage(msg, name string) {
+	for _, con := range Users {
+		// message := []byte(fmt.Sprintf("[%s]: %s", name, msg))
+		con.Write([]byte("kteb hadxi and kolxi"))
 	}
 }
